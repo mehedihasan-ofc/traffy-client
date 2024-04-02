b71427079da521d2c8556ea3a532802cbf3fc1f9
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2024-04-02 13:26:05 +0600

    Added Services

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Hero/Hero";
 import Partners from "../Partners/Partners";
+import Services from "../Services/Services";
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Partners />
+            <Services />
         </>
     );
 };
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,14 @@
+const Services = () => {
+    return (
+        <div className="my-container my-10">
+            
+            <div className="grid grid-cols-4 gap-5">
+                <div className="col-span-3">Services List</div>
+                <div className="col-span-1">Ads</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Services;
